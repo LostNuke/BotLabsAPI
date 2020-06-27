a39366654a1084057b1b8552fa5231f223145273
LostNuke <36674771+LostNuke@users.noreply.github.com>
2020-06-27 22:11:58 +0200

    Update BotLabs.js

--- BotLabs.js
+++ BotLabs.js
@@ -11,10 +11,6 @@ class BotLabs {
     var options = {
       'method': 'GET',
       'url': `https://bots.discordlabs.org/v2/bot/${BotID}`,
-      'headers': {
-        'x-api-key': 'bd2bde9c-02c8-49d1-a45d-bad46fabbf5d',
-        'Cookie': '__cfduid=d0d403fc2427b12bf7c1b1f78caabe2d31592937598'
-      }
     };
     let result = await new Promise(function (resolve, reject) {
       request(options, function (error, response) {
@@ -34,9 +30,7 @@ class BotLabs {
         'method': 'POST',
         'url': `https://bots.discordlabs.org/v2/bot/${this.client.user.id}/stats`,
         'headers': {
-          'x-api-key': 'bd2bde9c-02c8-49d1-a45d-bad46fabbf5d',
           'Content-Type': 'application/x-www-form-urlencoded',
-          'Cookie': '__cfduid=d0d403fc2427b12bf7c1b1f78caabe2d31592937598'
         },
         form: {
           'token': this.token,
